8d07704230b30e9ddca33368427172dd259864d7
Vlad <vlad@example.com>
2026-05-21 12:02:21 +1000

    Switch to Railway API

--- app/src/api.js
+++ app/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://mebelmir-production.up.railway.app/api';
 
 export async function getProducts() {
   const response = await fetch(`${API_URL}/products/`);
